6c37a03bd52f6839c2de4fc4a29620c2afd49150
sivadharshini <147294876+sivadharshininandhakumar@users.noreply.github.com>
2024-06-19 14:17:53 +0530

    visual studio

---
+++ multithread.java
@@ -0,0 +1,55 @@
+class MultithreadingDemo extends Thread
+{
+public void run()
+{
+try
+{
+System.out.println("Thread " + Thread.currentThread().getId()+ " is running");
+}
+
+catch (Exception e)
+{
+System.out.println("Exception is caught");
+}
+}
+}
+
+class Multithread
+{
+public static void main(String[] args)
+{
+int n = 8;
+for (int i = 0; i < n; i++)
+{
+MultithreadingDemo object = new MultithreadingDemo();
+object.start();
+}
+}
+}
+
+class MultithreadingDemo implements Runnable
+{
+public void run()
+{
+try {
+System.out.println( "Thread " + Thread.currentThread().getId() + " is running");
+}
+
+catch (Exception e)
+{
+System.out.println("Exception is caught");
+}
+}
+}
+
+class Multithread
+{
+public static void main(String[] args)
+{
+int n = 8; 
+for (int i = 0; i < n; i++)
+{
+Thread object = new Thread (new MultithreadingDemo());
+object.start ();
+}
+}}
